30e932927b8b4e27a458a944c6776587b6f91779
Smoz <Steve.Moseley@g.austincc.edu>
2017-08-08 17:27:36 -0500

    Created MinimumAndAveragingRevisited solution.

---
+++ MinimumAndAveragingRevisited/MinimumAndAveragingRevisited/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimumAndAveragingRevisited
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Set up the array.
+            int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+            // Determine the minimum.
+            int currentMinimum = Int32.MaxValue;
+            foreach (int number in array)
+            {
+                if (number < currentMinimum) { currentMinimum = number; }
+            }
+            Console.WriteLine("Minimum: " + currentMinimum);
+
+            // Determine the average value of the array.
+            int total = 0;
+            foreach (int number in array)
+            {
+                total += number;
+            }
+            float average = (float)total / array.Length;
+            Console.WriteLine("Average: " + average);
+
+            Console.ReadKey();
+
+        }
+    }
+}
