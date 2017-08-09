4cd657fd3fef87ae1cfbf10b5d3a9ddd87533499
Smoz <Steve.Moseley@g.austincc.edu>
2017-08-09 09:27:28 -0500

    ReversingAnArray

--- ReversingAnArray/ReversingAnArray/Program.cs
+++ ReversingAnArray/ReversingAnArray/Program.cs
@@ -10,6 +10,52 @@ namespace ReversingAnArray
     {
         static void Main(string[] args)
         {
+            int[] numbers = GenerateNumbers();
+            ReverseNumbers(numbers);
+            PrintNumbers(numbers);
+
+            Console.ReadKey();
         }
+
+        static int[] GenerateNumbers()
+        {
+            int[] numbers = new int[10];
+
+            for (int index = 0; index < 10; index++)
+            {
+                // The index starts at 0 but I want to start
+                // with 1 so I'm adding 1 here.
+
+                numbers[index] = index + 1;
+            }
+
+            return numbers;
+        }
+
+        static void PrintNumbers(int[] numbers)
+        {
+            for (int index = 0; index < numbers.Length; index++)
+            {
+                Console.WriteLine(numbers[index] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void ReverseNumbers(int[] numbers)
+        {
+            int firstIndex = 0;
+            int secondIndex = numbers.Length - 1;
+
+            while (firstIndex < secondIndex)
+            {
+                int temp = numbers[firstIndex];
+                numbers[firstIndex] = numbers[secondIndex];
+                numbers[secondIndex] = temp;
+
+                firstIndex++;
+                secondIndex--;
+            }
+        }
+        
     }
 }
