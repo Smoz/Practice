9af177d80dd28eb415a4650d8c54850b692d4358
Smoz <Steve.Moseley@g.austincc.edu>
2017-08-09 09:16:55 -0500

    Added method practice

---
+++ MethodPractice/MethodPractice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BeforeCounting();
+            CountToTen();
+            AfterCounting();
+            Console.ReadKey();
+        }
+
+        static void CountToTen()
+        {
+            for(int index = 1; index <= 10; index++)
+            {
+                Console.WriteLine(index);
+            }
+            
+        }
+
+        static void BeforeCounting()
+        {
+            Console.WriteLine("Hello! I'm greeting you from the 'Greeting Method' before I start counting!");
+        }
+        static void AfterCounting()
+        {
+            Console.WriteLine("I'm done counting and am inside the 'AfterCounting' Method! Goodbye!");
+        }
+    }
+}
---
+++ ReturningSomethingFromAMethod/ReturningSomethingFromAMethod/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturningSomethingFromAMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int playerScore = CalculatePlayerScore();
+            Console.WriteLine(playerScore);
+            GetNumberFromUser();
+            Console.ReadKey();
+        }
+
+        static int GetNumberFromUser()
+        {
+            int usersNumber = 0;
+
+            while (usersNumber < 1 || usersNumber > 10)
+            {
+                Console.WriteLine("Enter a number between 1 and 10: ");
+                string usersResponse = Console.ReadLine();
+                usersNumber = Convert.ToInt32(usersResponse);
+            }
+
+            return usersNumber;
+        }
+
+        static int CalculatePlayerScore()
+        {
+            int livesLeft = 3;
+            int underlingsDestroyed = 17;
+            int minionsDestroyed = 4;
+            int bossesDestroyed = 1;
+            
+
+            // If the player is out of lives, they lose all their points.
+            if(livesLeft == 0)
+            {
+                return 0;
+            }
+
+            // Otherwise the player gets 10 points per underling destroyed, 100 points
+            // per minion, and 1000 points per boss.
+            return underlingsDestroyed * 10 + minionsDestroyed * 100 + bossesDestroyed * 1000;
+        }
+    }
+}
