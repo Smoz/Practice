cdeb76a4fcf72700c855a3ec906dd4344a61540e
Smoz <Steve.Moseley@g.austincc.edu>
2017-08-10 15:31:24 -0500

    A simple program to test my understanding of methods and passing parameters.

---
+++ NamePrinter/NamePrinter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamePrinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleRunning();
+        }
+
+        static void SampleRunning()
+        {
+            string name = RequestName();
+            Console.WriteLine();
+            NamePrinting(name);
+            Console.ReadKey();
+        }
+
+        static string RequestName()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static void NamePrinting(string name)
+        {
+            Console.WriteLine("Nice to meet you " + name + "!");
+        }
+    }
+}
