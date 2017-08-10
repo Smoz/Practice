78e55cb8b7eb583abdd9a312c0cece211e1bee76
Smoz <Steve.Moseley@g.austincc.edu>
2017-08-10 14:58:25 -0500

    Fibonacci Sequence up to 10 numbers

--- FibonacciSequence/FibonacciSequence/Program.cs
+++ FibonacciSequence/FibonacciSequence/Program.cs
@@ -11,8 +11,29 @@ namespace FibonacciSequence
         static void Main(string[] args)
         {
 
+            for (int index = 1; index <= 10; index++)
+            {
+                Console.WriteLine(Fibonacci(index));
+            }
 
+            Console.ReadKey();
 
+        }
+
+        /// <summary>
+        /// Returns a number from the Fibonacci sequence, starting at 1.
+        /// Note that this implementation is not very optimized, and can
+        /// take a very long time if you're looking up large numbers.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        /// 
+        static ulong Fibonacci(int number)
+        {
+            if (number == 1) { return 1; }
+            if (number == 2) { return 1; }
+
+            return Fibonacci(number - 1) + Fibonacci(number - 2);
         }
     }
 }
