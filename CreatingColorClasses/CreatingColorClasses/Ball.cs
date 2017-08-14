f1b2f7f55cbaf93ca6cb3dae22a02ddc49c95766
Smoz <Steve.Moseley@g.austincc.edu>
2017-08-14 15:54:19 -0500

    Testing the classes by creating a color and ball class.

---
+++ CreatingColorClasses/CreatingColorClasses/Ball.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingColorClasses
+{
+    class Ball
+    {
+        private Color color;
+
+        private float radius;
+
+        private int timesThrown;
+
+        public Ball(Color color, float radius)
+        {
+            this.color = color;
+            this.radius = radius;
+            this.timesThrown = 0;
+        }
+
+        public void Pop()
+        {
+            radius = 0;
+        }
+
+        public void Throw()
+        {
+            if (radius > 0)
+            {
+                timesThrown++;
+            }
+        }
+
+        public int GetTimesThrown()
+        {
+            return timesThrown;
+        }
+    }
+}
---
+++ CreatingColorClasses/CreatingColorClasses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingColorClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Ball bigRed = new Ball(new Color(255, 0, 0), 5);
+            Ball littleBlue = new Ball(new Color(0, 0, 255), 3);
+
+            bigRed.Throw();
+            bigRed.Throw();
+            bigRed.Throw();
+            bigRed.Throw();
+
+            Console.WriteLine(bigRed.GetTimesThrown());
+
+            bigRed.Throw();
+            bigRed.Throw();
+            Console.WriteLine(bigRed.GetTimesThrown());
+
+            littleBlue.Throw();
+            littleBlue.Throw();
+            Console.WriteLine(littleBlue.GetTimesThrown());
+
+            littleBlue.Pop();
+            littleBlue.Throw();
+            Console.WriteLine(littleBlue.GetTimesThrown());
+
+            Console.ReadKey();
+
+        }
+    }
+}
