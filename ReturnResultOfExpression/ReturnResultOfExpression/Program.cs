ccda36ed00a9c2eceaaf8a8ae39d11972f2d561d
Smoz <Steve.Moseley@g.austincc.edu>
2017-08-18 15:28:43 -0500

    Return the result of an expression using only variables/value pairs.

---
+++ ReturnResultOfExpression/ReturnResultOfExpression/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnResultOfExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 7;
+            int c = 3;
+            int d = 14;
+
+            int result = a + b + c - d;
+
+            Console.Write(result);
+            Console.ReadKey();
+
+        }
+    }
+}
