b0a01c077e29af63c776b044335911f2755db1db
Smoz <Steve.Moseley@g.austincc.edu>
2017-08-18 15:20:58 -0500

    Remainder Calculator (Given the numerator and denominator)

---
+++ RemainderCalculator/RemainderCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemainderCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numerator = 0;
+            int denominator = 0;
+            
+
+            Console.Write("Enter a numerator: ");
+            numerator = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Enter a denominator: ");
+            denominator = Convert.ToInt32(Console.ReadLine());
+
+            int remainder = numerator % denominator;
+            Console.Write("The remainder is: " + remainder);
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+        }
+    }
+}
